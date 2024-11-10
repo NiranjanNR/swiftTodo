701c2ab78860b4f5ec35f3520bc9215d9edf359a
NiranjanNR <kidkool8@gmail.com>
2024-11-10 23:59:17 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Niranjan Ramachandran on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
